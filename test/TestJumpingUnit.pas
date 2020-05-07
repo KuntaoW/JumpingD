8e68880959fa9337ad26de6dfeda278fd954787e
KuntaoW <64619703+KuntaoW@users.noreply.github.com>
2020-05-07 19:00:40 +1000

    Test First Element Is 0
    Testcase first, should be one failed

--- test/TestJumpingUnit.pas
+++ test/TestJumpingUnit.pas
@@ -23,6 +23,10 @@ type
     [TestCase('Test Array Lenth 100','0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0;True',';')]
     [TestCase('Test Array Lenth 101','0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0;False',';')]
     procedure TestArrayLength(sArray:String; _result:boolean);
+
+    [TestCase('Test First Element Is 0','0,0;True',';')]
+    [TestCase('Test First Element Is 0','1,0;False',';')]
+    procedure TestFirstElementIs0(sArray:String; _result:boolean);
   end;
 
 implementation
@@ -38,6 +42,13 @@ begin
 end;
 
 procedure TestJumping.TestArrayLength(sArray:String; _result:boolean);
+var r: boolean;
+begin
+  r := aTJumping.Jump(0, sArray);
+  Assert.AreEqual(_result, r);
+end;
+
+procedure TestJumping.TestFirstElementIs0(sArray:String; _result:boolean);
 var r: boolean;
 begin
   r := aTJumping.Jump(0, sArray);
